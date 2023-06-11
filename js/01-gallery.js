c74daa922fdd7d37d9e9f2cb605fc9ad1febf84a
AleksandraCherevko <119119431+AleksandraCherevko@users.noreply.github.com>
2023-06-11 19:26:36 +0200

    update 1 task

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,84 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const imagesContainer = document.querySelector(".gallery");
+
+const cardsMarkup = createGalleryCardsMarkup(galleryItems);
+
+imagesContainer.insertAdjacentHTML("beforeend", cardsMarkup);
+
+imagesContainer.addEventListener("click", imageClick);
+
+function createGalleryCardsMarkup(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+      <li class="gallery__item">
+        <a class="gallery__link" href="${original}">
+          <img
+            class="gallery__image"
+            src="${preview}"
+            data-source="${original}"
+            alt="${description}"
+          />
+        </a>
+      </li>`;
+    })
+    .join("");
+}
+
+function imageClick(evt) {
+  blockStandartAction(evt);
+  if (evt.target.nodeName !== "IMG") {
+    return;
+  }
+  console.log(evt.target);
+
+  // const modalElement = document.querySelector(".modal");
+  // modalElement.addEventListener("click", () => {
+  //   instance.close();
+  // });
+
+  // REPETA
+  // function onImagesContainerClick(evt) {
+  //   blockStandartAction(evt);
+  //   const isImageSwatchEl = evt.target.classList.contains("gallery__item");
+
+  //   if (!isImageSwatchEl) {
+  //     return;
+  //   }
+
+  //   console.log(evt.target);
+
+  // basiclightbox , https://basiclightbox.electerious.com/
+
+  // const instance = basicLightbox.create(`
+  //     <img src="${evt.dataset.sourse}" width="800" height="600">
+  // `);
+
+  // instance.show();
+
+  //   function onImagesContainerClick(evt) {
+  //     blockStandartAction(evt);
+  //     if (evt.target.nodeName !== "IMG") {
+  //       return;
+  //     }
+
+  const instance = basicLightbox.create(`
+    <img src="${evt.target.dataset.sourse}" width="800" height="600">
+`);
+
+  instance.show();
+
+  imagesContainer.addEventListener("keydown", (evt) => {
+    if (evt.code === "Escape") {
+      instance.close();
+    }
+  });
+}
+
+function blockStandartAction(evt) {
+  evt.preventDefault();
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,14 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+
+// function onImagesContainerClick(evt) {
+//   evt.preventDefault();
+//   const options = {
+//     captionsDara: "alt",
+//     caprionDelay: 250,
+//   };
+//   const lightbox = new SimpleLightbox(".gallery a", options);
+//   window.removeEventListener("click", onGalleryItemClick);
+// }
