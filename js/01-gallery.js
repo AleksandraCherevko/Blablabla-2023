758bff9400b395faa6f2ea021d89f9c9fe29d264
AleksandraCherevko <119119431+AleksandraCherevko@users.noreply.github.com>
2023-06-12 13:23:53 +0200

    update task 1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -13,7 +13,7 @@ imagesContainer.addEventListener("click", imageClick);
 
 function createGalleryCardsMarkup(galleryItems) {
   return galleryItems
-    .map(({ preview, original, description }) => {
+    .map(({ original, preview, description }) => {
       return `
       <li class="gallery__item">
         <a class="gallery__link" href="${original}">
@@ -36,38 +36,8 @@ function imageClick(evt) {
   }
   console.log(evt.target);
 
-  // const modalElement = document.querySelector(".modal");
-  // modalElement.addEventListener("click", () => {
-  //   instance.close();
-  // });
-
-  // REPETA
-  // function onImagesContainerClick(evt) {
-  //   blockStandartAction(evt);
-  //   const isImageSwatchEl = evt.target.classList.contains("gallery__item");
-
-  //   if (!isImageSwatchEl) {
-  //     return;
-  //   }
-
-  //   console.log(evt.target);
-
-  // basiclightbox , https://basiclightbox.electerious.com/
-
-  // const instance = basicLightbox.create(`
-  //     <img src="${evt.dataset.sourse}" width="800" height="600">
-  // `);
-
-  // instance.show();
-
-  //   function onImagesContainerClick(evt) {
-  //     blockStandartAction(evt);
-  //     if (evt.target.nodeName !== "IMG") {
-  //       return;
-  //     }
-
-  const instance = basicLightbox.create(`
-    <img src="${evt.target.dataset.sourse}" width="800" height="600">
+  const instance = basicLightbox.create(` <div class="modal">
+    <img src="${evt.target.dataset.source}" width="800" height="600"> </div>
 `);
 
   instance.show();
@@ -82,3 +52,33 @@ function imageClick(evt) {
 function blockStandartAction(evt) {
   evt.preventDefault();
 }
+
+// const modalElement = document.querySelector(".modal");
+// modalElement.addEventListener("click", () => {
+//   instance.close();
+// });
+
+// REPETA
+// function onImagesContainerClick(evt) {
+//   blockStandartAction(evt);
+//   const isImageSwatchEl = evt.target.classList.contains("gallery__item");
+
+//   if (!isImageSwatchEl) {
+//     return;
+//   }
+
+//   console.log(evt.target);
+
+// basiclightbox , https://basiclightbox.electerious.com/
+
+// const instance = basicLightbox.create(`
+//     <img src="${evt.dataset.sourse}" width="800" height="600">
+// `);
+
+// instance.show();
+
+//   function onImagesContainerClick(evt) {
+//     blockStandartAction(evt);
+//     if (evt.target.nodeName !== "IMG") {
+//       return;
+//     }
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,15 +1,36 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
 
+const imagesContainer = document.querySelector(".gallery");
 
-// function onImagesContainerClick(evt) {
-//   evt.preventDefault();
-//   const options = {
-//     captionsDara: "alt",
-//     caprionDelay: 250,
-//   };
-//   const lightbox = new SimpleLightbox(".gallery a", options);
-//   window.removeEventListener("click", onGalleryItemClick);
-// }
+const cardsMarkup = createGalleryCardsMarkup(galleryItems);
+
+imagesContainer.insertAdjacentHTML("beforeend", cardsMarkup);
+
+imagesContainer.addEventListener("click", imageClick);
+
+function createGalleryCardsMarkup(galleryItems) {
+  return galleryItems
+    .map(({ original, preview, description }) => {
+      return `
+      <li class="gallery__item">
+        <a class="gallery__link" href="${original}">
+      <img class="gallery__image" src="${preview}" alt="${description}" />
+   </a>
+   </li>`;
+    })
+    .join("");
+}
+
+
+function imageClick(evt) {
+  evt.preventDefault();
+  const options = {
+    captionsData: "alt",
+    captionDelay: 250,
+  };
+  const lightbox = new SimpleLightbox(".gallery a", options);
+  window.removeEventListener("click", imageClick);
+}
